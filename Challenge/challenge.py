be1ec5f280bb2bd42dd275e494acf4e86770a2fe
Abdul Rehman Arif <coolabdulrehman3030@gmail.com>
2023-08-03 11:45:52 +0500

    final challenge

--- Challenge/challenge.py
+++ Challenge/challenge.py
@@ -11,6 +11,10 @@ for filepath in filepaths:
     filename = Path(filepath).stem
     pdf.add_page()
     pdf.set_font(family="Times", style='B', size=24)
-    pdf.cell(align='L', w=50, h=8, txt=f"{filename.title()}")
+    pdf.cell(align='L', w=50, h=8, txt=f"{filename.title()}", ln=1)
+    with open(filepath, 'r') as f:
+        lines = f.read()
+    pdf.set_font(family="Times", style='B', size=12)
+    pdf.multi_cell(w=190, h=8, txt=lines)
 
 pdf.output(f"PDFS/output.pdf")
